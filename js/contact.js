67f9fdc19620080cd2d1256552c0cdbfa0e48805
Bibek Moulik <36233718+bibekmoulik@users.noreply.github.com>
2018-02-10 02:04:13 +0530

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -31,7 +31,7 @@ function clearForm()
 	//document.getElementById("contactEmail").value = "";
 	//document.getElementById("contactNumber").value = "";
 	document.getElementById("contactMsg").value = "";
-	document.getElementById("contactMsg").placeholder = "Write your message here..";
+	document.getElementById("contactMsg").placeholder = "Write your message here.."; 
 }
 
 function sendMail()
